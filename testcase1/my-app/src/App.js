0c24e30a41a9db357cf8baf1fa58f88bae2fb82d
Nidhi H N <nidhinshetty28@gmail.com>
2024-07-03 11:57:04 +0530

    last commit

--- testcase1/my-app/src/App.js
+++ testcase1/my-app/src/App.js
@@ -1,13 +1,30 @@
-import logo from './logo.svg';
+// src/App.js
+import React, { useState } from 'react';
+import ProductList from './ProductList';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [topN, setTopN] = useState(5);
+
   return (
     <div className="App">
-      
+      <h1>Top {topN} Products</h1>
+      <ProductList topN={topN} />
+      <div>
+        <label>
+          Show Top N Products:
+          <input
+            type="number"
+            value={topN}
+            onChange={(e) => setTopN(Number(e.target.value))}
+            min="1"
+          />
+        </label>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
+
 
